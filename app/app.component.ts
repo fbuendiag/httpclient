ff82c7d2f145de3a5c658385693cf7dfdf6b2ebd
Félix Buendía García <fbuendia@trweb-pfc.disca.upv.es>
2018-05-01 10:36:12 +0200

    Commit message

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   template: `
+		<app-writer></app-writer>
+    `
+})
+export class AppComponent { 
+
+}
+    
